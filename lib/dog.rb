3957099e858fe4483acd7b786e7ef2cc14c34ea4
Asif Khan <akhan@codeinreach.com>
2019-07-20 21:51:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,6 @@
 class Dog  
-  attr_accessor :name
-  def initialize 
-    @name
   end
-end
 
-fido = Dog.new "Fido"
-snoopy = Dog.new "Snoopy"
-lassie = Dog.new "Lassie"
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
